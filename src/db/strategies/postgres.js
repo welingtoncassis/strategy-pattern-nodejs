0a2b6c3e46f341f4f7c0356b23db79f3cb281452
Welington Cruz de Assis <welingtoncassis@welingtoncassis.welingtoncassis>
2020-03-18 18:05:44 -0300

    Corrigindo metodo connect | Teste para CREATE

--- src/db/strategies/postgres.js
+++ src/db/strategies/postgres.js
@@ -1,15 +1,64 @@
-const ICrud = require('./../strategies/interfaces/interfaceCrud')
+const ICrud = require('./../strategies/interfaces/interfaceCrud');
+const Sequelize = require('sequelize');
 
 class Postgres extends ICrud {
-    constructor(){
+    constructor() {
         super()
+        this._driver = null;
+        this._herois = null;
     }
 
-    isConnected() {
-        
+    async isConnected() {
+        try {
+            await this._driver.authenticate()
+            return true
+        } catch (error) {
+            console.error('Connection Fail ', error)
+            return false;
+        }
     }
-    create(item) {
-        console.log('item salvo no postgres')
+
+    async create(item) {
+        const{ dataValues } = await this._herois.create(item)
+        return dataValues
+    }
+
+    async defineModel() {
+        this._herois = this._driver.define('heroes', {
+            id: {
+                type: Sequelize.INTEGER,
+                required: true,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            nome: {
+                type: Sequelize.STRING,
+                required: true
+            },
+            poder: {
+                type: Sequelize.STRING,
+                required: true
+            }
+        }, {
+            tableName: 'TB_HEROIS',
+            freezeTableName: false,
+            timestamps: false
+        })
+        await this._herois.sync()
+    }
+    async connect() {
+        this._driver = new Sequelize(
+            'heroes',
+            'admin',
+            'admin',
+            {
+                host: 'localhost',
+                dialect: 'postgres',
+                quoteIdentifires: false,
+                operatorsAliases: false
+            }
+        )
+        await this.defineModel()
     }
 }
 
--- src/tests/postgresStrategy.test.js
+++ src/tests/postgresStrategy.test.js
@@ -4,10 +4,22 @@ const Context = require('../db/strategies/base/contextStrategy');
 
 const context = new Context(new Postgres())
 
+const MOCK_HEROI_CADASTRAR = { 
+    nome: 'Gavião Negro',
+    poder: 'flexas'
+}
 describe('Postgres Strategy', function(){
     this.timeout(Infinity);
+    this.beforeAll(async function() {
+        await context.connect()
+    })
     it('Postgres Connection', async function(){
         const result = await context.isConnected();
         assert.equal(result, true);
     } )
+    it('cadastrar', async function() {
+        const result = await context.create(MOCK_HEROI_CADASTRAR)
+        delete result.id
+        assert.deepEqual(result, MOCK_HEROI_CADASTRAR)
+    })
 })
